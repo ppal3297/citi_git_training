8ccb504fd99a0eb81235cdd1e6668cd1e233ef00
pankaj <pankaj@gmail.com>
2020-09-08 11:20:56 +0000

    modified 5 times run

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! new feature added."); 
-    }
+// Simple Java Program to Print Hello World
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
